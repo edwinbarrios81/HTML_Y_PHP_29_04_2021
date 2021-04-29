140fd763de774114e0ef5aac1554b31878df6b66
edwinbarrios81 <edwinbarrios81@gmail.com>
2021-04-29 17:11:52 -0500

    29-04-2021

---
+++ formulario.php
@@ -0,0 +1,50 @@
+<?php
+
+echo <<<Nombre
+tu Nombre es $_REQUEST[Nombre]
+Nombre;
+echo "<br>";
+
+echo <<<Apellido
+tu Apellido es $_REQUEST[Apellido]
+Apellido;
+echo "<br>";
+
+echo <<<Edad
+tu Edad es $_REQUEST[Edad]
+Edad;
+echo "<br>";
+
+
+echo <<<Estatura
+tu Edad es $_REQUEST[Estatura]
+Estatura;
+echo "<br>";
+
+echo <<<Telefono
+tu Edad es $_REQUEST[Telefono]
+Telefono;
+echo "<br>";
+
+echo <<<Genero
+tu Genero es $_REQUEST[Genero]
+Genero;
+echo "<br>";
+
+echo <<<Raza
+tu Raza es $_REQUEST[Raza]
+Raza;
+
+// var_dump ($_REQUEST);
+
+
+// echo "hola mundo este es el formulario php";
+// echo "<br>";
+// // echo $_POST ["apellido"];
+
+// // settype ($_REQUEST ["num1"], 'int') ;
+// // settype ($_REQUEST ["num2"], 'int') ;
+// // var_dump ($_REQUEST); +[$_REQUEST ["num2"]
+// var_dump ($_REQUEST);
+
+?>
